41cf2518b381a294072af8ea8e6dd01e18fec1e9
gab5987 <biell.gb4@icloud.com>
2022-12-11 16:57:41 -0300

    Feat: Main page and its counter

---
+++ src/Content.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Content extends React.Component {
+    constructor(props: never) {
+        super(props);
+    }
+
+    render() {
+        return <p> ukytiuyt</p>
+    }
+}
