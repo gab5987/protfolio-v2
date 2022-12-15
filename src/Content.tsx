fc65424f72c4cb418890c991002e194535a02dce
gab5987 <biell.gb4@gmail.com>
2022-12-15 16:36:34 -0300

    Feat: Content Page

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,4 +1,17 @@
 import React from "react";
+import "./App.scss";
+var background = require("./data/bg-img2.gif");
+
+const styleHeader = {
+    color: "white", 
+    fontSize: "calc(6px + 2vmin)",
+    textAlign: "center" as const,
+    webkitTextStroke: ".3px black",
+}
+
+const styleTitleSection = {
+    
+}
 
 export default class Content extends React.Component {
     constructor(props: never) {
@@ -6,6 +19,22 @@ export default class Content extends React.Component {
     }
 
     render() {
-        return <p> ukytiuyt</p>
+        return (
+            <>
+                <section className="App" style={{backgroundImage: `url(${background})`}}>
+                    <div style={styleHeader}>
+                        <h1>Gabriel Novalski</h1> <br />
+                        <h4>Software developer</h4>
+                    </div>
+                </section>
+
+                <section>
+                    <div className="title">
+                        <h1>Content</h1>
+                    </div>
+                </section>
+
+            </>
+        )
     }
 }
